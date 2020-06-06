7549379179bfb6de09123d48a024c3d9abbaafe4
Juan Espinoza <juan.espinoza@fluxit.com.ar>
2020-06-06 18:38:34 -0300

    Se inicia la página pública a través de Express

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    //res.send('Hello World')
-    let out = {
-        nombre: 'Juan',
-        edad: 26,
-        url: req.url
-    };
+//Se crea un Middleware qye llamará a un callback y
+//presentará la información por el HTML
+app.use(express.static(__dirname + '/public'));
 
-    res.send(out);
-});
+// app.get('/', (req, res) => {
+//     //res.send('Hello World')
+//     let out = {
+//         nombre: 'Juan',
+//         edad: 26,
+//         url: req.url
+//     };
+
+//     res.send(out);
+// });
 
 app.get('/data', (req, res) => {
     res.send('Hello World')
